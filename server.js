e94c1efcd39028348c2faaf9a069fce1dcf4853c
Chanchaljangir <chanchaljangir9@gmail.com>
2021-09-13 23:58:14 +0530

    curtains dynemic

--- server.js
+++ server.js
@@ -187,6 +187,17 @@ app.get('/bsmall', function (req, res) {
     });
   });
 });
+app.get('/curtains', function (req, res) {
+  let sql = `SELECT productid,c.categoryname, pt.producttypename,sm.sizename,productimgurl,price,brand,isnewarrival FROM homedecor.products as p inner join homedecor.category as c on c.categoryid=p.categoryid
+  inner join homedecor.producttype as pt on pt.producttypeid=p.producttypeid
+  inner join homedecor.sizemaster as sm on sm.sizeid=p.sizeid where producttypename="curtain"`;
+  let query = conn.query(sql, function (err, myresults) {
+    if (err) throw err;
+    res.render(__dirname + '/curtain', {
+      results: myresults
+    });
+  });
+});
 app.get('/ddouble', function (req, res) {
   res.render(__dirname + "/duvetdouble");
 });
@@ -196,9 +207,7 @@ app.get('/btnsignout', function (req, res) {
   res.render(__dirname + "/homapage1");
 });
 
-app.get('/curtains', function (req, res) {
-  res.render(__dirname + "/curtain");
-});
+
 
 app.get('/clocks', function (req, res) {
   res.render(__dirname + "/clocks");
